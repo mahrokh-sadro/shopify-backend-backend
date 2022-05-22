0c205f397b5053edab56943923d9a629116b8110
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-05-22 19:01:02 -0400

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const corsOptionsDelegate = function (req, callback) {
   const allowlist = [
     `http://localhost:3000`,
     "http://127.0.0.1:3000",
-    "https://frontend-inventory.netlify.app/",
+    "https://frontend-inventory.netlify.app",
   ];
   let corsOptions;
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
